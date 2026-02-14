d0e3829f351ad26d17c3e38650c754c19447f9a0
Mateo M <mateo.martinezr@uqvirtual.edu.co>
2026-02-14 09:56:30 -0500

    Add files via upload

--- Actividad1/src/main/java/co/edu/uniquindio/actividad1/Conversacion.java
+++ Actividad1/src/main/java/co/edu/uniquindio/actividad1/Conversacion.java
@@ -4,6 +4,7 @@ public class Conversacion {
     public static void main(String[] args) {
         System.out.println("Mateo: Quiubo bo ya esta hecho pa que podamos hablar");
         System.out.println("Nicolay: Mano, me daba problemas reabrir este codigo, pero, ya esta, se pudo");
+        System.out.println("Mateo: Bo a mi me dejo clonarlo desde el bash sin problemas, no fue sino borrar la que ya tenia y poner el comando");;
     }
 }
 
